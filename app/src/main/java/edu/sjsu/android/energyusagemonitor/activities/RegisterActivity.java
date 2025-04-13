c7df5ead246552a7db3957abf4c1996f2b2da566
Johnny Nguyen <jqbnguyen80@gmail.com>
2025-04-12 23:08:38 -0700

    Registration for new account and normal login implementation

--- app/src/main/java/edu/sjsu/android/energyusagemonitor/activities/RegisterActivity.java
+++ app/src/main/java/edu/sjsu/android/energyusagemonitor/activities/RegisterActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,10 +18,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import edu.sjsu.android.energyusagemonitor.R;
+import edu.sjsu.android.energyusagemonitor.databinding.ActivityLoginBinding;
+import edu.sjsu.android.energyusagemonitor.ui.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +30,10 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         final Button registerButton = findViewById(R.id.register_action);
+        final Button loginHereButton = findViewById(R.id.login_here);
         final EditText usernameEditText = findViewById(R.id.username_register);
         final EditText passwordEditText = findViewById(R.id.password_register);
+        final EditText passwordEditText2 = findViewById(R.id.password_register2);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -37,22 +42,38 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-                mAuth.createUserWithEmailAndPassword(username, password)
-                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Registration successful, move to HomeDashboardActivity
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    if (user != null) {
-                                        Intent intent = new Intent(RegisterActivity.this, HomeDashboardActivity.class);
-                                        startActivity(intent);
-                                        finish();
+                String password2 = passwordEditText2.getText().toString();
+                if (password.equals(password2)) {
+                    mAuth.createUserWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Registration successful, move to HomeDashboardActivity
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        if (user != null) {
+                                            Intent intent = new Intent(RegisterActivity.this, HomeDashboardActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    } else {
+                                        showRegistrationFailed(R.string.registration_failed);
                                     }
                                 }
-                            }
-                        });
+                            });
+                }
+                else {
+                    showRegistrationFailed(R.string.password_no_match);
+                }
             }
         });
+
+        loginHereButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+    }
+
+    private void showRegistrationFailed(@StringRes int errorString) {
+        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 }
